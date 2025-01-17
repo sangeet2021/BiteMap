74aad409bf48242fc3b1580bcd8bcff9e1b8c7ac
sangeet2021 <stiletdurden252@gmail.com>
2025-01-17 23:26:48 +0545

    Adding List

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import "./List.css";
 
 const List = () => {
-  return <div className="list">Lissss</div>;
+  return (
+    <div className="list">
+      <div className="restaurant-header">
+        <h1>Restaurants Near You!</h1>
+        <div className="rating">
+            <label htmlFor="">Rating</label>
+            <select name="" id=""></select>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default List;
